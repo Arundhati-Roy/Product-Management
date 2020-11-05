29a0437f868aa37e20fbc38d5080c63064093eaa
Arundhati-Roy <arundhati.roy1998@gmail.com>
2020-11-05 09:11:28 +0530

    first commit

---
+++ Product Management Review/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data;
+
+namespace Product_Management_Review
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linq Demo");
+            DataTable t = new DataTable();
+            t.Columns.Add("ID");
+            t.Columns.Add("PName");
+
+            t.Rows.Add("1", "Chai");
+            t.Rows.Add("2", "Coffee");
+            t.Rows.Add("3", "Latte");
+            DisplayEmp(t);
+        }
+        public static void DisplayEmp(DataTable emp)
+        {
+            var empName = from employee in emp.AsEnumerable()
+                          select employee.Field<string>("PName");
+            foreach (string n in empName)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
